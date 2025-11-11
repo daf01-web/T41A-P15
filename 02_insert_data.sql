8e3e69ae1d7291a59a6739223e2007dc2314170f
daf01-web <171513@upslp.edu.mx>
2025-11-11 10:15:39 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO productos (nombre, precio, stock) VALUES 
+    ('Laptop', 1200.50, 16),
+    ('Teclado', 75.99, 40),
+    ('Mouse', 25.00, 5),
+    ('Monitor', 299.99, 58),
+    ('Silla', 150.75, 2),
+    ('Cafetera', 45.20, 23),
+    ('Audífonos', 89.90, 87),
+    ('Mochila', 55.00, 100),
+    ('Disco Duro', 110.00, 234),
+    ('Webcam', 68.50, 154),
+    ('Router', 99.99, 40),
+    ('Bocinas', 65.40, 56);
+
+INSERT INTO departamentos (nombre) VALUES ('Ventas'), ('Tecnología'), ('Recursos Humanos');
+
+INSERT INTO empleados (nombre, id_departamento) VALUES
+('Ana Gómez', 1),        
+('Luis Torres', 1), 
+('Carla Ruiz', 2),      
+('David Parra', 2),     
+('Elena Sánchez', 2);  
